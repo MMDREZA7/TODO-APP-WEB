3d4b2e0636948fbd1421e17d095a36f7ded0cd91
MMDREZA <mheidari831003.game@gmail.com>
2024-05-28 15:36:43 +0330

    TODO APP FINISHED!,

--- src/compenents/todo/Todo.jsx
+++ src/compenents/todo/Todo.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./todo.css";
 
-const Todo = ({ text, isDone, deleteTodo }) => {
+const Todo = ({ text, isDone, deleteTodo, renameTodo }) => {
   const [isDoing = isDone, setIsDoing] = useState(isDone);
 
   const changeIsDone = () => {
@@ -8,15 +10,37 @@ const Todo = ({ text, isDone, deleteTodo }) => {
   };
 
   return (
-    <div className="todo-container" onDoubleClick={deleteTodo}>
-      <h3>{text}</h3>
-      <input
-        className="checkBox"
-        type="checkbox"
-        value={isDoing}
-        checked={isDoing}
-        onChange={changeIsDone}
-      />
+    <div className="todo-container">
+      <div className="todo-checkbox-name">
+        {/* Check Box */}
+        <div className="todo-checkbox-container">
+          <input
+            className="checkBox-todo"
+            type="checkbox"
+            value={isDoing}
+            checked={isDoing}
+            onChange={changeIsDone}
+          />
+        </div>
+        {/* Name */}
+        <div className="todo-name-container">
+          <h3 className="todo-name">{text}</h3>
+        </div>
+      </div>
+      <div className="todo-btns">
+        {/* Delete */}
+        <div className="delete-btn-container">
+          <button onClick={deleteTodo} className="btn delete-btn">
+            ❌
+          </button>
+        </div>
+        {/* Rename */}
+        <div className="rename-btn-container">
+          <button onClick={renameTodo} className="btn rename-btn">
+            🖊
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/compenents/todos/Todos.jsx
+++ src/compenents/todos/Todos.jsx
@@ -10,53 +10,75 @@ const Todos = () => {
   ]);
 
   const [newTodoText, setNewTodoText] = useState("");
-  const [newTodoIsDone, setNewTodoIsDone] = useState(false);
+  const [newTodoName, setNewTodoName] = useState("");
+  const [state, setState] = useState(false);
+  const [editIndex, setEditIndex] = useState(null);
 
-  // F U N C T I O N S
-
-  // GET VALUE from TextInput
+  // Function to handle input change
   const changeTextInputValue = (event) => {
-    setNewTodoText(event.target.value);
+    state
+      ? setNewTodoName(event.target.value)
+      : setNewTodoText(event.target.value);
   };
 
-  // ADD NEW TODO
+  // Function to add a new todo
   const addNewTodo = (event) => {
     event.preventDefault();
 
-    // Add new Todo in list
-    const todo = { todoText: newTodoText, inDone: false };
+    // Add new todo to the list
+    const todo = { todoText: newTodoText, isDone: false };
     setTodoList([...todoList, todo]);
 
     // Reset the text input
     setNewTodoText("");
   };
 
-  // DELETE TODO
+  // Function to delete a todo
   const deleteTodo = (index) => {
     const updatedItems = [...todoList];
     updatedItems.splice(index, 1);
     setTodoList(updatedItems);
+  };
 
-    console.log("Deleted ✔");
+  // Function to rename a todo
+  const renameTodo = (event, index) => {
+    event.preventDefault();
+
+    const updatedItems = [...todoList];
+    updatedItems[index] = { ...updatedItems[index], todoText: newTodoName };
+    setTodoList(updatedItems);
+    setNewTodoName("");
+    setEditIndex(null);
+    setState(false);
+  };
+
+  const changeState = (index) => {
+    setEditIndex(index);
+    setState(true);
+    setNewTodoName(todoList[index].todoText);
   };
 
   return (
     <div className="todos-container">
       <h1 className="main-title">TODO Tracker</h1>
 
-      <form onSubmit={addNewTodo} className="add-todo-form">
+      {/* Add New Todo */}
+      <form
+        onSubmit={state ? (event) => renameTodo(event, editIndex) : addNewTodo}
+        className="add-todo-form"
+      >
         <div>
-          <label className="label">Todo Name</label>
+          <label className="label">{state ? "Rename Todo" : "Todo Name"}</label>
           <input
             className="new-todo-input"
             type="text"
-            placeholder="Enter Your Todo Name"
-            value={newTodoText}
+            placeholder={state ? "Enter New Name" : "Enter Your Todo Name"}
+            value={state ? newTodoName : newTodoText}
             onChange={(e) => changeTextInputValue(e)}
           />
         </div>
         <button type="submit" className="btn submit">
-          Add Todo
+          {state ? "Rename" : "Add Todo"}
         </button>
       </form>
       {todoList.map((todo, index) => (
@@ -65,6 +87,7 @@ const Todos = () => {
           isDone={todo.isDone}
           key={index}
           deleteTodo={() => deleteTodo(index)}
+          renameTodo={() => changeState(index)}
         />
       ))}
     </div>
